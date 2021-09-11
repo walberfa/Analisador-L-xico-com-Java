70d7946024f76252c2d612d2f3db9086de947ffb
Lapq <lapq2006@gmail.com>
2021-09-10 22:55:41 -0300

    Pegando arquivo de input por argumento na execução

--- src/main/java/analisadorLexico/Generator.java
+++ src/main/java/analisadorLexico/Generator.java
@@ -1,8 +1,13 @@
 package analisadorLexico;
 
-import jflex.exceptions.SilentExit;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Paths;
+
+import jflex.exceptions.SilentExit;
 
 
 public class Generator {
@@ -17,9 +22,19 @@ public class Generator {
 			e.printStackTrace();
 		}
 
+		if (Array.getLength(args) != 1) {
+			System.out.println("ERRO: informe apenas o parâmetro do nome do arquivo");
+			return;
+		}
+		
 		String inputPath = Paths.get("").toAbsolutePath().toString() + "\\exemplo\\";
-		File input = new File(inputPath + "input.txt");
-        LexicalAnalyzer lexical;
+		File input = new File(inputPath + args[0]);
+		if (!input.exists()) {
+			System.out.println("ERRO: arquivo não existe");
+			return;
+		}
+		
+		LexicalAnalyzer lexical;
 		try {
 			lexical = new LexicalAnalyzer(new FileReader(input));
 			try {
