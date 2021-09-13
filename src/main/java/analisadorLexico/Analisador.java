410f73b1fab83eff3e0e96e5d3c3410b84366d19
Lapq <lapq2006@gmail.com>
2021-09-13 14:41:20 -0300

    Create Analisador.java
    Agora esse é o arquivo principal para executar a classe gerada, enquanto o gerador.java é apropriadamente delegado apenas a gerar a classe para o desenvolvimento, não sendo necessário para inclusão no release final

---
+++ src/main/java/analisadorLexico/Analisador.java
@@ -0,0 +1,38 @@
+package analisadorLexico;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Paths;
+
+
+public class Analisador {
+
+	public static void main(String[] args) {
+
+		if (Array.getLength(args) != 1) {
+			System.out.println("ERRO: informe apenas o parâmetro do nome do arquivo");
+			return;
+		}
+
+		String inputPath = Paths.get("").toAbsolutePath().toString() + "\\";
+		File input = new File(inputPath + args[0]);
+		System.out.println(inputPath + args[0]);
+		if (!input.exists()) {
+			System.out.println("ERRO: arquivo não existe");
+			return;
+		}
+
+		LexicalAnalyzer lexicalAn;
+
+		try {
+			lexicalAn = new LexicalAnalyzer(new FileReader(input), args[0]);
+			lexicalAn.yylex();
+			lexicalAn.fecharOutput();
+		} catch (IOException e1) {
+			System.out.print("ERRO: ");
+			e1.printStackTrace();
+		}
+	}
+}
